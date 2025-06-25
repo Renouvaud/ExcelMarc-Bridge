b1020453785061f15285480d1ed835a523f99048
npayot <noemie.payot@bcu.unil.ch>
2025-06-26 01:49:02 +0200

    bugs fix (eval selon condition ou valTrue/valFalse, eval guillemets, échappement caractères utilisés dans json, remplacement retours à la ligne selon colonne)+ ajout fct today_date et format_date

--- complete_fields.py
+++ complete_fields.py
@@ -1,65 +1,216 @@
 """ Python libraries """
+from cmath import nan
+from multiprocessing import Value
 import xml.etree.ElementTree as ET
 import re
+from datetime import datetime
 
 """ Local functions """
 import global_dict
 
-"""def complete_field_008(record, map_008, el_type="mono", date1="    ", date2="    ", country="fre", lang="fr"):
-    c008 = ET.SubElement(record, 'controlfield')
-    c008.attrib = "008"
-    if el_type == "mono":
-        c008.text = f"      s{date1}{date2}    {country}|||||||||| 00| 0 z{lang} d"
-    return c008"""
+def rpl_json_spe_caracter(ET_el):
+    ET_el = ET_el.replace("<diese>", "#")
+    ET_el = ET_el.replace("<at>", "@")
+    return ET_el
 
-   
-def fct_split(pos, src):
-    return src.split()[pos]
+def get_ind(check_str):
+    if check_str != None:
+        return check_str.replace("_", " ")
+    return ' '
 
-def secure_eval(val):
-    if not val or val.strip()=='':
-        return val
-    return str(eval(val))
+def convert_space_caracter(val, replacement, reg_rpl, not_inc={}):
+    if len(not_inc)>0:
+        val = re.sub(" *[\n\t\r] *", replacement, val)
+        for not_search, not_rpl in not_inc.items():
+            val = re.sub(f"{not_search}{reg_rpl}", f"{not_rpl} ", val)
+    else:
+        val = re.sub(" *[\n\t\r] *", replacement, val)
+    val = re.sub(reg_rpl, replacement, val)
+    val = re.sub(f"^{reg_rpl}", "", val)
+    val = re.sub(f"{reg_rpl}$", "", val)
+    return val
 
-def var_substitute(row, arg):
-    match = re.search('%([a-zA-Z_0-9]+)', arg)
+def convert_special_caracter(val, spe_car_list, remplacement):
+    for spe_car in spe_car_list:
+        match_spe = re.search(f"{spe_car}", val)
+        if match_spe != None:
+            val = val.replace(match_spe.group(), remplacement)
+    return val
+
+def var_substitute(row, arg, cond=False):
+    list_arg = []
+    list_arg.append(arg.split("@")[0])
+    if len(arg.split("@"))>1:
+        for mot in arg.split("@")[1:]:
+            mot = f"@{mot}"
+            match = re.search("@([a-zA-Z_0-9]+)", mot)
+            col_name = f"{match.group(1)}"
+            search_el = match.group()
+            col_val = str(row[col_name])
+            if col_val == 'nan':
+                col_val = ''
+            """if col_name == "Age_min" and "5" in col_val:
+                print(col_val)
+                breakpoint()"""
+            # Remplacement de " par ' --> toujours même délémitations pour citation
+            col_val = convert_special_caracter(col_val, ['"'], "'")
+            # Remplacement des retours à la ligne et tab en fonction de la colonne
+            if re.search("(contenu)|(505)", col_name.lower()) and not col_val.startswith("[Sommaire]"):
+                col_val = convert_space_caracter(col_val, " -- ", "( -- )+")
+            elif re.search("r[éeè]sum[éeè]r?", col_name.lower()) or (re.search("som+aire", col_name.lower()) and col_val.startswith("[Sommaire]")):
+                col_val = convert_space_caracter(col_val, " - ", "( - )+")
+            else :
+                col_val = convert_space_caracter(col_val, ". ", "(\. )+", not_inc={"\?":"?", "\.":".", "!":"!", ":":":", ",":",", ";":";"})            
+            if cond:
+                # Pour condition attention à remplace les citations dans les données
+                col_val = convert_special_caracter(col_val, ["'"], " ")
+                list_arg.append(mot.replace(search_el, f"'{col_val}'"))
+            else:
+                list_arg.append(mot.replace(search_el, col_val))
+    return "".join(list_arg)  
+
+def glob_eval(el):
+    if not el:
+        return el
+    m_guillemet = re.search("global_dict\.[a-zA-Z_0-9]+(\[)[a-zA-Z_0-9\.,_\-]+(\])", el)
+    if m_guillemet:
+        el = el.replace(m_guillemet.group(1), "['")
+        el = el.replace(m_guillemet.group(2), "']")
+    match = re.search("global_dict\.[a-zA-Z_0-9]+((\['[a-zA-Z_0-9\.,_\-]+'\])|(\.((keys)|(values)|(items))\(\)))?", el)
     if not match:
-        return arg
-    col_name = match.group(1)
-    search_el = match.group()
-    return arg.replace(search_el, str(row[col_name]))      
+        return el
+    try:
+      return el.replace(match.group(), str(eval(match.group())))    
+    except:
+        breakpoint()
 
-def glob_substitute(arg):
-    match = re.search('(#)[a-zA-Z_0-9]+', arg)
-    if not match:
-        return arg
-    return arg.replace(match.group(1), "global_dict.")
+def glob_substitute(arg, cond=False):
+    list_arg = []
+    list_arg.append(arg.split("#")[0])
+    if len(arg.split("#"))>1:
+        for mot in arg.split("#")[1:]:
+            mot = f"global_dict.{mot}"
+            if cond:
+                list_arg.append(mot)
+            else:
+                mot = glob_eval(mot)
+                list_arg.append(mot)
+                #match = re.search("global_dict\.[a-zA-Z_0-9]+((\['[a-zA-Z_0-9,]+'\])|(\.((keys)|(values)|(items))\(\)))?", mot)
+                #list_arg.append(mot.replace(match.group(), str(eval(match.group()))))
+            #match = re.search('(#)[a-zA-Z_0-9]+', mot)
+            #list_arg.append(mot.replace(match.group(1), "global_dict."))
+    return "".join(list_arg)
 
 def fct_if(condition, valTrue, valFalse=""):
     global last_if
+    if "0,5" in condition:
+        breakpoint()
     last_if = None
     if eval(condition):
         last_if = True
-        return secure_eval(valTrue)    
+        return glob_eval(valTrue)    
     last_if = False
-    return secure_eval(valFalse)
+    return glob_eval(valFalse)
 
 def fct_elif(condition, valTrue, valFalse=""):
+    global last_if
     if last_if:
         return ""
     if eval(condition):
-        return secure_eval(valTrue)
-    return secure_eval(valFalse)
+        last_if = True
+        return glob_eval(valTrue)
+    return glob_eval(valFalse)
+
+"""def fct_search():
+    pass
+
+def fct_format_date():
+    pass
+
+def fct_today_date():
+    pass
+
+def fct_split(pos, src):
+    return src.split()[pos]"""
+
+def map_value(row, value, cond_var=False, cond_glob=False):
+    mapped_value = var_substitute(row, value, cond_var)
+    mapped_value = glob_substitute(mapped_value, cond_glob)
+    return mapped_value
+
+def apply_fct(row, funcargs, value):
+    func_list = ["if", "elif"] 
+    if any(funcargs[0].startswith(func) for func in func_list):
+        arg_list = []
+        arg_list.append(map_value(row, funcargs[1], cond_var=True, cond_glob=True))
+        [arg_list.append(map_value(row, arg, cond_var=False, cond_glob=True)) for arg in funcargs[2:]]
+        func = "fct_" + funcargs[0]
+        #'fct_if':fct_if, 'fct_elif':fct_elif, 'fct_search':fct_search, 'fct_split':fct_split, 'fct_format_date':fct_format_date, 'fct_today_date':fct_today_date
+        return eval(func, {'fct_if':fct_if, 'fct_elif':fct_elif})(*arg_list) #arguments donnés sous forme de liste
+    return map_value(row, value)
+
+
+def convert_date(date_str, d_format_entree, d_format_sortie):
+    # Convertir la chaîne en objet datetime
+    try:
+        date_obj = datetime.strptime(date_str, d_format_entree)
+    except:
+        return ""
+    # Changer le format en DD.MM.YYYY
+    try :
+        date_formatee = date_obj.strftime(d_format_sortie)
+    except:
+        return ""
+    return date_formatee
+
+
+def fct_format_date(row, content, reg_date):
+    m_format_date = re.search(f"foramt_date\((@[a-zA-Z0-9]+), ({reg_date}), ({reg_date})\)", content)
+    if not m_format_date:
+        print("Erreur dans format_date")
+        print("ex : format_date(@col_name, <format date entrée>, <format date sortie>)")
+        print("exemple : format_date(@Date_achat, %a %b %d %H:%M:%S %Z %Y, %Y-%m-%d)")
+        print("détails des formats de date\n%a : Jour de la semaine abrégé (ex. : Mon)\n%b : Mois abrégé (ex. : Nov)\n%d : Jour du mois (ex. : 01)\n%H : Heure (format 24 heures, ex. : 00)\n%M : Minutes (ex. : 00)\n%S : Secondes (ex. : 00)\n%Z : Fuseau horaire (ex. : CET)\n%Y : Année avec siècle (ex. : 1993)")
+        exit()
+    value = var_substitute(row, m_format_date.group(1), cond=False)
+    new_date = convert_date(value, m_format_date.group(2), m_format_date.group(3))
+    return content.replace(m_format_date.group(), new_date)
+
+def fct_today_date(content, reg_date):
+    m_format_date = re.search(f"today_date\(({reg_date})\)", content)
+    if not m_format_date:
+        print("Erreur dans today_date")
+        print("ex : today_date(<format date>)")
+        print("exemple : today_date(%m.%Y)")
+        print("détails des formats de date\n%a : Jour de la semaine abrégé (ex. : Mon)\n%b : Mois abrégé (ex. : Nov)\n%d : Jour du mois (ex. : 01)\n%H : Heure (format 24 heures, ex. : 00)\n%M : Minutes (ex. : 00)\n%S : Secondes (ex. : 00)\n%Z : Fuseau horaire (ex. : CET)\n%Y : Année avec siècle (ex. : 1993)")
+        exit()
+    try:
+        new_date = datetime.now().strftime(m_format_date.group(1))
+    except:
+        return "Erreur today_date ; format date en entrée invalide"        
+    return content.replace(m_format_date.group(), new_date)
 
 def convert_field_content(row, func_str):
-    func_list = ["for", "if", "elif", "search", "split", "format_date","today_date"]
-    mapped_value = func_str    
-    if "|" in func_str:
-        funcargs = func_str.split('|')
-        if any(funcargs[0].startswith(func) for func in func_list):
-            args = funcargs[1:]
-            args = [var_substitute(row, arg) for arg in args]
-            args = [glob_substitute(arg) for arg in args]
-            func = "fct_" + funcargs[0]
-            mapped_value = eval(func, {'fct_if':fct_if, 'fct_elif':fct_elif})(*args) #arguments donnés sous forme de liste
-    return mapped_value
+    fct_date_list = ["format_date","today_date"]
+    for fct_date in fct_date_list:
+        if fct_date in func_str:
+            reg_date = "[%abdHMSZYm\.\-:_ ]+"
+            if fct_date == "format_date":
+                func_str = fct_format_date(row, func_str, reg_date)
+            elif fct_date == "today_date":
+                func_str = fct_today_date(func_str, reg_date)
+    if "§" in func_str:
+        funcargs = func_str.split('§')
+        return apply_fct(row, funcargs, func_str)
+    return map_value(row, func_str)
+
+def add_subfield(datafield, row, code, func_str):
+    subfield = ET.SubElement(datafield, 'subfield', attrib={'code': code})
+    subfield.text = convert_field_content(row, func_str)
+    subfield.text = rpl_json_spe_caracter(subfield.text)
+
+def message_erreur_for(tag, sf):
+    print("ERREUR arguments pour tag {tag} dans sf {sf}")
+    print("Lorsque 'for' est utilisé, il faut au moins 2 arguments, exemple : 'for§boucle§contenu boucle")
+    print("Note : pour utliser l'élément courant dans le contenu de la boucle, utiliser %s")
+    print("Note : il est possible d'ajouter une condition dans la boucle comme suit : for§boucle§if§condition§alors§sinon")
--- main_v01.py
+++ main_v01.py
@@ -11,6 +11,7 @@ from datetime import *
 import os
 from os import listdir #, walk
 from os.path import isfile, isdir, join
+import re
 
 # Calls function main()
 if __name__ == "__main__":
@@ -27,64 +28,86 @@ if __name__ == "__main__":
     glob_dict = gen_params['global_dict']
     define_glob_dict(glob_dict)
 
-    xlsx_f_name = f"{gen_params['xlsx_file_input']}.xlsx"
-    excel_file = os.path.join(os.getcwd(), xlsx_f_name)
-
     # Import nom fichier de sortie
     xml_output_file = f"{gen_params['xml_file_output']}.xml"
 
+    # Import fichier excel
+    xlsx_f_name = f"{gen_params['xlsx_file_input']}.xlsx"
+    excel_file = os.path.join(os.getcwd(), xlsx_f_name)
     # Ouverture fichier excel
-    df = pd.read_excel(excel_file)
+    df = pd.read_excel(excel_file, dtype=str)
 
     # Créer la racine XML
     collection = ET.Element('collection', {
         'xmlns:marc': "http://www.loc.gov/MARC21/slim",
         'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance"
     })
-
+    # Pour chaque ligne du fichier excel, création d'un record
+    printed_col = []
     for _, row in df.iterrows():
         record = ET.SubElement(collection, 'record')
-        """leader = ET.SubElement(record, 'leader')
-        leader.text = '00000nam a2200000   4500'
 
-        fields_by_tag = {}
+        # Remplacement des carctères spéciaux utilisés dans gen_params.json dans la ligne traitée
+        for row_key, row_val in row.items():
+            if type(row_val)==str and re.search("[#@]", row_val):
+                row_val = row_val.replace("#", "<diese>")
+                row_val = row_val.replace("@", "<at>")
+                row[row_key] = row_val
+            if row_key not in printed_col and row_val not in ["nan", "", None, nan, 0]:
+                print(f"{row_key}\t\t{row_val}")
+                printed_col.append(row_key)
 
-        # Regroupement des sous-champs par tag
-        for col, val in row.items():
-            if pd.notnull(val) and col in mapping.keys():
-                tag_code = mapping[col]  # e.g. "245_a"
-                tag, code = tag_code.split('_')
-                fields_by_tag.setdefault(tag, []).append((code, str(val)))"""
+        # Pour chaque élément de la liste mapping définie dans le fichier json
         for map_field in mapping:
 
-            # ajout leader au record
+            # Ajout leader au record
             if not isinstance(map_field[1], list):
                 record_el = ET.SubElement(record, map_field[0])
                 record_el.text = map_field[1]
 
-            # ajout controlfields 00X au record
+            # Ajout controlfields 00X au record
             elif map_field[0].startswith("00"):
                 controlfield = ET.SubElement(record, "controlfield")
                 controlfield.attrib = attrib={'tag': map_field[0]}
                 controlfield_list = []
                 for pos in map_field[1:]:
+                    if pos[0] == "_comment":
+                        continue
                     temp_val = convert_field_content(row, pos[1])
                     controlfield_list.append(temp_val)
                 controlfield.text = "".join(controlfield_list)
 
-            # ajout datafields
-            match_tag = re.search('([0-9]{3})(([0-9_#])([0-9_#]))?')
+            # Ajout datafields au record
             elif re.search('[0-9]{3}[0-9_#]{0,2}', map_field[0]):
-                b
+                match_tag = re.search('([0-9]{3})(([0-9_#])([0-9_#]))?', map_field[0])
+                tag = match_tag.group(1)
+                ind1 = get_ind(match_tag.group(3))
+                ind2 = get_ind(match_tag.group(4))
+                datafield = ET.Element('datafield', attrib={'tag': tag, 'ind1': ind1, 'ind2': ind2})
 
-                
-        # Ajout des datafields
-        """for tag, subfields in fields_by_tag.items():
-            datafield = ET.Element('datafield', attrib={'tag': tag, 'ind1': ' ', 'ind2': ' '})
-            for code, value in subfields:
-                sf = ET.SubElement(datafield, 'subfield', attrib={'code': code})
-                sf.text = value
-            record.append(datafield)"""
+                for sf in map_field[1:]:
+                    if sf[0] == "_comment":
+                        continue
+                    if sf[1].startswith("for§"):
+                        # exemple de sf avec for : ["a", "for§'@Langue'.split('/')§if§%s in #lang008.keys§#lang008[%s]"]
+                        # sf[1] = "for§'@Langue'.split('/')§if§%s in #lang008.keys§#lang008[%s]"
+                        sf1_list = sf[1].split('§') # = "for", "'@Langue'.split('/')", "if", "%s in #lang008.keys§#lang008[%s]"
+                        boucle = sf1_list[1] # = '@Langue'.split('/')
+                        mapped_value = map_value(row, sf1_list[1], cond_var=True, cond_glob=True) # = 'EN/FR/DE'.split('/')
+                        # cette boucle permet de créer plusieurs sous-champs avec le même code en bouclant sur une liste
+                        # exemple : 041 $a eng $a fre $a ger
+                        for el in eval(mapped_value):
+                            if el == None or el == '':
+                                continue
+                            if len(sf1_list)<2:
+                                message_erreur_for(tag, sf)
+                                exit()
+                            func_str = "§".join(sf1_list[2:])
+                            add_subfield(datafield, row, sf[0], func_str.replace("%s", f"'{el}'"))
+                    else:
+                        add_subfield(datafield, row, sf[0], sf[1])
+                record.append(datafield)            
+            #ET.dump(record)
 
     # Génération du fichier XML
     tree = ET.ElementTree(collection)
